e9d82453c2aa19d78ae763e6e7ca7a886135e8a0
Andrew Binder <andrewdbinder@gmail.com>
2020-02-10 23:52:04 -0800

    Add Graph to history page

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -15,7 +15,7 @@ con.connect(function (err) {
 });
 
 router.get('/', function (req, res, next) {
-    con.query("SELECT * FROM powermonitoring.powerusage", function (err, result, fields) {
+    con.query("SELECT * FROM powermonitoring.powerusage WHERE MeasurementTime BETWEEN CAST('2020-02-06' AS DATE) AND CAST('2020-02-07' AS DATE);", function (err, result, fields) {
         // if (err) throw err;
         // console.log(result);
         console.log('Querying PowerUsage Table');
